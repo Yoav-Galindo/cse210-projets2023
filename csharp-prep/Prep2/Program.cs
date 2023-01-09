59ce0c432534ab650f55bb8319202c1e1fa721b4
Yoav-Galindo <gal22005@byui.edu>
2023-01-09 14:30:43 -0700

    porcentage grade program 1st commit

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Welcome, what is your porcentage grade? ");
+        int userPorcentageGrade = Console.Read();
+
+        if (userPorcentageGrade >= 90)
+        {
+            Console.Write("You got A grade");
+        }
+
+        else if(userPorcentageGrade >= 80)
+        {
+            Console.Write("You got B grade");
+        }
+
+        else if(userPorcentageGrade >= 70)
+        {
+            Console.Write("You got C grade");
+        }
+
+        else if(userPorcentageGrade >= 60)
+        {
+            Console.Write("You got D grade");
+        }
+        
+        else if(userPorcentageGrade < 60)
+        {
+            Console.Write("You got F grade");
+        }
+
+        else
+        {
+            Console.Write("This is a not valid porcentage.")
+        }
+
     }
 }
+
